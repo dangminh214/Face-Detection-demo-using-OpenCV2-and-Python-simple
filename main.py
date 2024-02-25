88ec0dec049efaf238c2821a9c6d902bcefbbced
dangminh214 <dangminh214@gmail.com>
2024-02-25 17:19:06 +0100

    fix small bug when choose an legit image

--- main.py
+++ main.py
@@ -9,30 +9,32 @@ def choose_image():
     # open dialog to choose image
     file_path = filedialog.askopenfilename()
 
-    # check if user has already selected        
-    if cv2.imread(file_path):
-        image = cv2.imread(file_path)
-        height, width, _ = image.shape
-        if (height < 800 and width < 800):
-            new_width = int(width * 1.5)
-            new_height = int(height * 1.5)
-        elif (height > 1000 and width > 1000):
-            new_width = int(width * 0.5)
-            new_height = int(height * 0.5)
-        else: 
-            new_width = width
-            new_height = height
+    if file_path: 
 
-        image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
-        print('width: ', new_width, 'heigth: ', new_height)
-        print('path: ', file_path)
-        face_detect(image)
+        # check if user has already selected       
+        image = cv2.imread(file_path) 
+        if image is not None:
+            height, width, _ = image.shape
+            if (height < 800 and width < 800):
+                new_width = int(width * 1.5)
+                new_height = int(height * 1.5)
+            elif (height > 1000 and width > 1000):
+                new_width = int(width * 0.5)
+                new_height = int(height * 0.5)
+            else: 
+                new_width = width
+                new_height = height
 
-    else:
-        print("No image was selected.")
-        show_popup('Fail', 'No image was selected.')
-        root.quit()
-        return None
+            image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+            print('width: ', new_width, 'heigth: ', new_height)
+            print('path: ', file_path)
+            face_detect(image)
+
+        else:
+            print("No image was selected.")
+            show_popup('Fail', 'No image was selected.')
+            root.quit()
+            return None
      
 
 def show_popup(title, content):
